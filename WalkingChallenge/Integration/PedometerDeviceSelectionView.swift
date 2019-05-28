2c15a5b665a26a4ace5f5634b24352cd0378287a
huseinkareem <huskar95@icloud.com>
2019-05-28 13:18:48 -0500

    create pedometerdevicedelectionview

---
+++ WalkingChallenge/Integration/PedometerDeviceSelectionView.swift
@@ -0,0 +1,14 @@
+ //
+//  PedometerDeviceSelectionView.swift
+//  WalkingChallenge
+//
+//  Created by Husein Kareem on 5/26/19.
+//  Copyright © 2019 AKDN. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+ class PedometerDeviceSelectionView: UIViewController {
+  
+ }
